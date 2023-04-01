081afb435253bac4fb3e2ee00a965d66f048f904
Jhonatan Cardona <tatancarduar@gmail.com>
2023-04-01 14:15:04 -0500

    Sexto commit useEffect y consumo de APIs

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products'
 
 const ProductList = () => {
+	const [products, setProduct] = useState([]);
+
+	useEffect (async() => {
+		const response = await axios(API)
+		setProduct(response.data);
+	},{})
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(products => (
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
