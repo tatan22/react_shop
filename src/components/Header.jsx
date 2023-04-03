ea5731e106c4ecaa540d40e9f8b8858818727662
Jhonatan Cardona <tatancarduar@gmail.com>
2023-04-03 18:52:43 -0500

    Decimo commit Carrito de compra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,26 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
-import Menu from '@components/Menu.jsx'
+import Menu from '@components/Menu';
+import MyOrder from '../containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
-import AppContext from '../context/AppContext.js'
+import AppContext from '../context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
-	const [toggle, setToogle] = useState(false);
-	const {state} = useContext(AppContext);
+	const [toggle, setToggle] = useState(false);
+	const [toggleOrders, setToggleOrders] = useState(false);
+	const { state } = useContext(AppContext);
+
 	const handleToggle = () => {
-		setToogle(!toggle);
+		setToggle(!toggle);
 	}
+
 	return (
 		<nav>
 			<img src={menu} alt="menu" className="menu" />
 			<div className="navbar-left">
-				<img src={logo} alt="logo" className="logo" />
+				<img src={logo} alt="logo" className="nav-logo" />
 				<ul>
 					<li>
 						<a href="/">All</a>
@@ -43,13 +47,17 @@ const Header = () => {
 					<li className="navbar-email" onClick={handleToggle}>
 						platzi@example.com
 					</li>
-					<li className="navbar-shopping-cart">
+					<li
+						className="navbar-shopping-cart"
+						onClick={() => setToggleOrders(!toggleOrders)}
+					>
 						<img src={shoppingCart} alt="shopping cart" />
 						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
 					</li>
 				</ul>
 			</div>
-			{toggle && <Menu/>}
+			{toggle && <Menu />}
+			{toggleOrders && <MyOrder />}
 		</nav>
 	);
 }
